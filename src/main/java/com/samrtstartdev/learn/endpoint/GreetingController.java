34f8d637ba5ea2616989b11405a84bcbf220b741
sarsargsyan <sarsargsyan@gmail.com>
2016-10-12 16:33:58 -0700

    start  a build

--- src/main/java/com/samrtstartdev/learn/endpoint/GreetingController.java
+++ src/main/java/com/samrtstartdev/learn/endpoint/GreetingController.java
@@ -15,6 +15,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
+    int a= 1;
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
